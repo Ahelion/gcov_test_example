972749874b39df7bb77e0d032b7b01aeef002328
Mircea Cociuba <mircea.cociuba@wipro.com>
2022-03-06 11:32:32 +0200

    initial algo code

--- Sources/src/algo.c
+++ Sources/src/algo.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
 #include "algo.h"
+/*
+    Functionality:
+    given a specific string it creates motorola srec:
+    https://en.wikipedia.org/wiki/SREC_(file_format)
+*/
+
+static uint8_t algo_string_ptr[ALGO_MAX_SIZE_DATA];
+
+static uint8_t* algo_global_string_ptr=NULL;
+
+static algo_srec_t algo_srec_line_st;
+
+static algo_states_t algo_state_en=ALGO_NONE;
 
 int Algo_GetVersion(uint32_t *ver)
 {
@@ -16,4 +30,51 @@ int Algo_GetVersion(uint32_t *ver)
     }
 
     return retVal;
+}
+
+void Algo_MainFuncion(void)
+{
+    switch(algo_state_en)
+    {
+    case ALGO_NONE:
+         break;
+    case ALGO_INIT:
+         break;
+    case ALGO_STARTED:
+         break;
+    case ALGO_FINISHED:
+          break;
+    case ALGO_ERROR:
+          break;
+    default:
+          break;
+    }
+}
+
+int Algo_SetPointerToString(uint8_t* string_ps)
+{
+    if(NULL!=string_ps)
+    {
+        uint32_t size=strlen(string_ps);
+        //only 64 characters allowed
+        if(size<=ALGO_MAX_SIZE_DATA*2)
+        {
+            memcpy(algo_string_ptr,string_ps,size-1);
+        }
+    }
+}
+
+int Algo_Compute(void)
+{
+    
+}
+
+int Algo_GetResultToString(uint8_t* outString)
+{
+
+}
+
+algo_states_t Algo_GetState(void)
+{
+    return algo_state_en;
 }
--- Sources/src/algo.h
+++ Sources/src/algo.h
@@ -1,8 +1,33 @@
 
 
+/*defines*/
 #define ALGO_TRUE 1
 #define ALGO_FALSE 0
 
-#define ALGO_VER 0xCAFECAFE
+#define ALGO_VER 0xCAFECAFEu
 
+#define ALGO_MAX_SIZE_DATA 32u
+
+/*prototypes*/
 extern int Algo_GetVersion(uint32_t *ver);
+
+/*types*/
+
+typedef struct
+{
+    uint8_t    record_type_u8;
+    uint8_t    byte_count_u8;
+    uint32_t   address_u32;
+    uint8_t    data[ALGO_MAX_SIZE_DATA];
+    uint8_t    crc;
+}algo_srec_t;
+
+typedef enum
+{
+    ALGO_NONE       =0,
+    ALGO_INIT       =1,
+    ALGO_STARTED    =2,
+    ALGO_FINISHED   =3,
+    ALGO_ERROR      =4,
+    ALGO_UNDEF      =0xFF
+}algo_states_t;
